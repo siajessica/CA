f3da1a51afa3a3c3c52def07d62d0985b281a9b0
b08902083 <b08902083@linux12.csie.ntu.edu.tw>
2021-05-02 02:06:50 +0800

    correctgit add .!

--- HW4/CA2021_hw4/codes/Adder.v
+++ HW4/CA2021_hw4/codes/Adder.v
@@ -1,14 +1,14 @@
 module Adder(
-    data1_i, 	
-    data2_i,	
+    data1_in, 	
+    data2_in,	
     data_o		
 );
 
 // Ports
-input [31:0] data1_i;
-input [31:0] data2_i;
+input [31:0] data1_in;
+input [31:0] data2_in;
 output [31:0] data_o;
 
-assign data_o = data1_i + data2_i;
+assign data_o = data1_in + data2_in;
 
 endmodule
